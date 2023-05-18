01c983f2e1d49516024374f247c5486a5bc771a4
Chirag Khanna <ckhanna@horizontal.com>
2023-05-18 14:02:45 +0530

    initial commit for xm cloud

--- src/sxastarter/src/components/FeaturedBlog.tsx
+++ src/sxastarter/src/components/FeaturedBlog.tsx
@@ -25,6 +25,7 @@ type FeaturedBlog = {
 };
 
 type PromoProps = {
+  params: { [key: string]: string };
   fields: {
     data: {
       item: {
@@ -37,25 +38,35 @@ type PromoProps = {
   };
 };
 
+const FeaturedBlogDefaultComponent = (props: PromoProps): JSX.Element => (
+  <div className={`component promo ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Featured Blog</span>
+    </div>
+  </div>
+);
+
 export const Default = (props: PromoProps): JSX.Element => {
   console.log('GraphQL Json');
   console.log(props);
-
-  return (
-    <div className="row component">
-      <h1>
-        <Text className="" field={props.fields.data.item.heading} />
-      </h1>
-      {props.fields.data.item.featuredblogs.blogs.map((item) => {
-        return (
-          <div className="col-4" key={item.title.value}>
-            <Text className="promo-text" field={item.title} />
-            <JssRichText field={item.description} />
-            <a href={`/${item.link.url}`}>{item.link.text}</a>
-            <JssImage field={item.image.jsonValue} />
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (props && props.fields && props.fields.data && props.fields.data.item) {
+    return (
+      <div className="row component">
+        <h1>
+          <Text className="" field={props.fields.data.item.heading} />
+        </h1>
+        {props.fields.data.item.featuredblogs.blogs.map((item) => {
+          return (
+            <div className="col-4" key={item.title.value}>
+              <Text className="promo-text" field={item.title} />
+              <JssRichText field={item.description} />
+              <a href={`/${item.link.url}`}>{item.link.text}</a>
+              <JssImage field={item.image.jsonValue} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  return <FeaturedBlogDefaultComponent {...props} />;
 };
--- src/sxastarter/src/components/FeaturedBlogResolver.tsx
+++ src/sxastarter/src/components/FeaturedBlogResolver.tsx
@@ -21,31 +21,42 @@ type FeaturedBlogFields = {
 };
 
 type PromoProps = {
+  params: { [key: string]: string };
   fields: {
     Heading: Field<string>;
     items: FeaturedBlogFields[];
   };
 };
 
+const FeaturedBlogResolverDefaultComponent = (props: PromoProps): JSX.Element => (
+  <div className={`component promo ${props.params.styles}`}>
+    <div className="component-content">
+      <span className="is-empty-hint">Featured Blog Resolver</span>
+    </div>
+  </div>
+);
+
 export const Default = (props: PromoProps): JSX.Element => {
   console.log('Resolver Json');
   console.log(props);
-
-  return (
-    <div className="row component">
-      <h1>
-        <Text className="" field={props.fields.Heading} />
-      </h1>
-      {props.fields.items.map((item) => {
-        return (
-          <div className="col-4" key={item.fields.Title.value}>
-            <Text className="promo-text" field={item.fields.Title} />
-            <JssRichText field={item.fields.Description} />
-            <JssLink field={item.fields.Link} />
-            <JssImage field={item.fields.Image} />
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (props.fields) {
+    return (
+      <div className="row component">
+        <h1>
+          <Text className="" field={props.fields.Heading} />
+        </h1>
+        {props.fields.items.map((item) => {
+          return (
+            <div className="col-4" key={item.fields.Title.value}>
+              <Text className="promo-text" field={item.fields.Title} />
+              <JssRichText field={item.fields.Description} />
+              <JssLink field={item.fields.Link} />
+              <JssImage field={item.fields.Image} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  return <FeaturedBlogResolverDefaultComponent {...props} />;
 };
